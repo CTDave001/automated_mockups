40e00faf2f0513e11eb9ecd2046433c32f10b52a
CTDave <88465578+CTDave001@users.noreply.github.com>
2023-08-10 08:52:39 -0700

    Update calculate_box_pos.py

--- calculate_box_pos.py
+++ calculate_box_pos.py
@@ -9,24 +9,27 @@ from skimage.color import rgb2gray
 from skimage.measure import label, regionprops
 
 def calculate_parameters(image_path, target_color):
-    image = io.imread(image_path)
+    image = io.imread(image_path)[:, :, :3]
     grayscale = rgb2gray(image)
 
     r, g, b = target_color
     target_gray = 0.2989*r + 0.5870*g + 0.1140*b
     mask = grayscale == target_gray
-    
+
     label_img, _ = label(mask, connectivity=mask.ndim, return_num=True)
     properties = regionprops(label_img)
-    
-    for prop in properties:
-        if prop.area == np.max([p.area for p in properties]):
-            bbox = prop.bbox
-            height = bbox[2]-bbox[0]
-            width = bbox[3]-bbox[1]
-            rotation = prop.orientation
 
-    return bbox, height, width, rotation
+    if properties:
+        for prop in properties:
+            if prop.area == np.max([p.area for p in properties]):
+                bbox = prop.bbox
+                height = bbox[2]-bbox[0]
+                width = bbox[3]-bbox[1]
+                rotation = prop.orientation
+        return bbox, height, width, rotation
+    else:
+        print("No properties found for image: " + image_path)
+        return
 
 def save_parameters(directory, target_color):
     parameters = {}
@@ -34,7 +37,8 @@ def save_parameters(directory, target_color):
         if filename.endswith(".png") or filename.endswith(".jpg"):  
             image_path = os.path.join(directory, filename)
             bbox, height, width, rotation = calculate_parameters(image_path, target_color)
-            parameters[filename] = {"bbox": bbox, "height": height, "width": width, "rotation": rotation}
+            if bbox and height and width:
+                parameters[filename] = {"bbox": bbox, "height": height, "width": width, "rotation": rotation}
 
     with open("parameters.json", "w") as file:
         json.dump(parameters, file)
@@ -42,8 +46,14 @@ def save_parameters(directory, target_color):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate parameters for positioning designs on product mockups.")
     parser.add_argument('--color', type=int, nargs=3, metavar=('R', 'G', 'B'), help="The RGB values of the color to find in the images.")
+    parser.add_argument('--hex_color', type=str, help="The hexadecimal value of the color to find in the images.")
     parser.add_argument('--dir', type=str, default='images/box_mockups', help="Directory of images with the box.")
     args = parser.parse_args()
 
-    target_color = tuple(args.color)
+    if args.hex_color:
+        hex_color = args.hex_color.lstrip('#')
+        target_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2 ,4))
+    else:
+        target_color = tuple(args.color)
+        
     save_parameters(args.dir, target_color)
